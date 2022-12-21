a02d38d3064ecd0cd3ffaa2ff84cce466aa587f3
rohitkishne <rohitkishne3@gmail.com>
2022-12-21 20:25:53 +0530

    Initial Commit

---
+++ src/student_result_management_system/Student_Result_Management_System.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package student_result_management_system;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author dell
+ */
+public class Student_Result_Management_System {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame frame = new JFrame();
+        frame.setTitle("STRESUMS Portal");
+    }
+    
+}
