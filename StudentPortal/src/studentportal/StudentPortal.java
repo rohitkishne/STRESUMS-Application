8edec357e7b5d3c75d7bb1a9f4d553ba50ace98f
rohitkishne <92744319+rohitkishne@users.noreply.github.com>
2023-02-19 23:45:57 +0530

    first update

---
+++ StudentPortal/src/studentportal/StudentPortal.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package studentportal;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author dell
+ */
+public class StudentPortal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setTitle("STRESUMS PORTAL");
+    }
+    
+}
